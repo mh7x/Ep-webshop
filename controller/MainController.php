f6066e359473de3d462c28cea90abd5feba205c5
kkeroo <jasko10.kerec@gmail.com>
2021-12-23 13:14:06 +0100

    dodan prikaz izdelkov iz baze

--- controller/MainController.php
+++ controller/MainController.php
@@ -6,24 +6,12 @@ require_once("ViewHelper.php");
 class MainController {
 
     public static function index() {
-        echo ViewHelper::render("view/index.php", [
-            "articles" => ArticleDB::getAll()
-        ]);
+        $data = ArticleDB::getAll();
+        echo ViewHelper::render("view/index.php", ["articles" => $data]);
     }
 
     public static function product() {
-        $rules = [
-            "id" => [
-                'filter' => FILTER_VALIDATE_INT,
-                'options' => ['min_range' => 1]
-            ]
-        ];
-
-        $data = filter_input_array(INPUT_GET, $rules);
-
-        echo ViewHelper::render("view/product.php", [
-            "article" => ArticleDB::get($data)
-        ]);
+        echo ViewHelper::render("view/product.php");
     }
 
     public static function cart() {
@@ -42,40 +30,18 @@ class MainController {
         echo ViewHelper::render("view/signup.php");
     }
 
-    public static function controlPanel() {
-        echo ViewHelper::render("view/control-panel.php", [
-            "articles" => ArticleDB::getAll()
-        ]);
-    }
-
-    /**
-     * Returns TRUE if given $input array contains no FALSE values
-     * @param type $input
-     * @return type
-     */
-    private static function checkValues($input) {
-        if (empty($input)) {
-            return FALSE;
-        }
-
-        $result = TRUE;
-        foreach ($input as $value) {
-            $result = $result && $value != false;
-        }
-
-        return $result;
-    }
-
-    /**
-     * Returns an array of filtering rules for manipulation books
-     * @return type
-     */
-    private static function getRules() {
-        return [
-            'title' => FILTER_SANITIZE_SPECIAL_CHARS,
-            'description' => FILTER_SANITIZE_SPECIAL_CHARS,
-            'price' => FILTER_VALIDATE_FLOAT,
+    public static function verifySignIn() {
+        $rules = [
+            "email" => [
+                'filter' => FILTER_VALIDATE_EMAIL
+            ],
+            "password" => [
+                'filter' => FILTER_VALIDATE_STRING
+            ]
         ];
-    }
 
+        $filteredData = filter_input_array(INPUT_POST, $rules);
+        var_dump($filteredData);
+        
+    }
 }
