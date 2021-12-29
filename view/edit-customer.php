6708622ea94879ebcb574deaef5ee5204f328d23
kkeroo <jasko10.kerec@gmail.com>
2021-12-29 12:32:58 +0100

    Lepsi izgled pri urejanju stranke.

--- view/edit-customer.php
+++ view/edit-customer.php
@@ -8,11 +8,11 @@
 
 
         <!-- Bootstrap CSS -->
-        <link href="../../public/css/style.css" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-              integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-
+        <link href="../public/css/style.css" rel="stylesheet" type="text/css"/>
+        
 
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <title>Urejanje stranke</title>
@@ -65,6 +65,10 @@
         </section>
 
         <?php include("layout/footer.php") ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     </body>
 
 </html>
